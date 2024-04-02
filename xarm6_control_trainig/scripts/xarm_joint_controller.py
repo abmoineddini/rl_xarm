6c0761bc9d5d66860ffec17fb0386b30992577e7
Amirbahador Moineddini <abmoineddini@gmail.com>
2024-04-02 17:29:26 +0100

    adding the xarm-controller node

--- xarm6_control_trainig/scripts/xarm_joint_controller.py
+++ xarm6_control_trainig/scripts/xarm_joint_controller.py
@@ -2,8 +2,8 @@
 import sys
 from xarm_msgs.srv import PlanExec
 from xarm_msgs.srv import PlanJoint
-from std_msgs.msg import Empty
-from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from std_msgs.msg import Float32MultiArray
+import numpy as np
 
 import rclpy
 from rclpy.node import Node
@@ -13,12 +13,12 @@ from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 
 class ResetSim(Node):
     def __init__(self):
-        super().__init__('reset_sim_node')
+        super().__init__('xarm_joint_controller_node')
 
         topic_group = MutuallyExclusiveCallbackGroup()
         plan_group = MutuallyExclusiveCallbackGroup()
         excute_group = MutuallyExclusiveCallbackGroup()
-        self.subscription_colour_image = self.create_subscription(Empty, 'reset_sim', self.callback, 10, callback_group=topic_group)
+        self.subscription_colour_image = self.create_subscription(Float32MultiArray, 'xarm_joint_controller', self.callback, 10, callback_group=topic_group)
         
         self.execute_joint_pos = self.create_client(PlanExec, '/xarm_exec_plan', callback_group= plan_group)
         self.plan_joint_pos = self.create_client(PlanJoint, '/xarm_joint_plan', callback_group= excute_group)
@@ -26,25 +26,13 @@ class ResetSim(Node):
         self.req_plan = PlanJoint.Request()
         self.req_excute = PlanExec.Request()
 
-        # Reseting the ball Pose
-        self.publisher_ = self.create_publisher(JointTrajectory, 'set_joint_trajectory', 10)        
-        trajectory_point = JointTrajectoryPoint()
-        trajectory_point.positions = [0.0, 0.0, 0.0]  # Joint positions
-        trajectory_point.velocities = [0.0, 0.0, 0.0]  # Joint velocities
-        trajectory_point.accelerations = [0.0, 0.0, 0.0]  # Joint accelerations
-        self.msg = JointTrajectory()
-        self.msg.header.frame_id = 'world'
-        self.msg.joint_names = ['theta_joint', 'r_joint', 'z_joint']
-        self.msg.points.append(trajectory_point)
 
     def send_execute_request(self):
         self.req_excute.wait = True     
         return self.execute_joint_pos.call(self.req_excute)
     
-    def send_plan_request(self, ):
-        target_initial_pose = [-8.0, -55, -18.0, 5.0, 46.0, -11.0]
-        print([x * 0.01745329251 for x in target_initial_pose])
-        self.req_plan.target = [x * 0.01745329251 for x in target_initial_pose]
+    def send_plan_request(self, joint_angle):
+        self.req_plan.target = joint_angle
         response = self.plan_joint_pos.call(self.req_plan)
         if response.success==True:
             response = self.send_execute_request()
@@ -53,15 +41,12 @@ class ResetSim(Node):
             print("planning failed")
         return response
     
-    def callback(self, Null_msg):
-            response = self.send_plan_request()
-            print(f"group response = {response.success}")
-            if response.success==True:
-                self.publisher_.publish(self.msg)
-        
-
-        
-        # if result.sucess:
+    def callback(self, msg):
+        print(msg.data)
+        print(type(msg.data))
+        action =msg.data
+        response = self.send_plan_request(action.tolist())
+        print(f"group response = {response.success}")
 
 
 def main(args=None):
